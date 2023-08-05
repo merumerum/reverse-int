01563c175cb5551c73081456c23d9e47d4ec36f8
Meruert <meru.forwork@gmail.com>
2023-08-05 14:09:45 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if (Math.sign(n) === 1) {
+    let res = n.toString().split('').reverse().join('');
+    return res;
+  } else {
+    n = Math.abs(n);
+    let res = n.toString().split('').reverse().join('');
+    return res;
+  }
 }
+
+// let result = reverse(-854);
+// console.log(result);
+
+
+
+
